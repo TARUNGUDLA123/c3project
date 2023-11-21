0f21a2c5b52fb4db4a3de74a2ab258205695259d
TARUNGUDLA123 <laxmigudla1977@gmail.com>
2023-11-21 12:15:57 +0530

    Intial commit

---
+++ Restaurant_project -Student version/src/test/java/RestaurantServiceTest1.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantServiceTest1 {
+
+    @Test
+    void findRestaurantByName() {
+    }
+
+    @Test
+    void addRestaurant() {
+    }
+
+    @Test
+    void removeRestaurant() {
+    }
+
+    @Test
+    void getRestaurants() {
+    }
+}
